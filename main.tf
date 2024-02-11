8da8d9e33dc753ef67d29b5a1f3f6e70d737896b
amu94777 <amu94777@gmail.com>
2024-02-11 15:57:59 +0530

    jenkins piiline

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-resource "aws_lb_target_group" "component" {
-  name     = "${local.name}-${var.tags.Component}"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  deregistration_delay = 60
-  health_check {
-      healthy_threshold   = 2
-      interval            = 10
-      unhealthy_threshold = 3
-      timeout             = 5
-      path                = "/health"
-      port                = 8080
-      matcher = "200-299"
-  }
-}
+# resource "aws_lb_target_group" "component" {
+#   name     = "${local.name}-${var.tags.Component}"
+#   port     = 8080
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id
+#   deregistration_delay = 60
+#   health_check {
+#       healthy_threshold   = 2
+#       interval            = 10
+#       unhealthy_threshold = 3
+#       timeout             = 5
+#       path                = "/health"
+#       port                = 8080
+#       matcher = "200-299"
+#   }
+# }
 
 module "component" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
